62ff96a60c9b1a72ddac60933b3a80298d5f8388
FilipKorev <filipkorev95@gmail.com>
2023-06-18 23:05:15 +0200

    2023.06.08

---
+++ Chapter10/WorkingWithEFCore/Program.Queries.cs
@@ -0,0 +1,55 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Packt.Shared;
+
+partial class Program
+{
+    static void QueryingCategories()
+    {
+        using (Northwind db = new Northwind())
+        {
+            SectionTitle("Categproes and how many products they have");
+
+            IQueryable<Category> categories = db.Categories?
+                .Include(c => c.Products);
+
+            if((categories is null) || (!categories.Any()))
+                {
+                Fail("no categories found!");
+                return;
+            }
+
+            foreach (Category category in categories)
+            {
+                Console.WriteLine($"{category.CategoryName} has {category.Products.Count} products");
+                Console.WriteLine("They are:");
+                foreach (var product in category.Products)
+                {
+                    Console.WriteLine($" * {product.ProductName}");
+                }
+            }
+        }
+    }
+
+    static void QueryingProducts()
+    {
+        using (Northwind db = new Northwind())
+        {
+            SectionTitle("Product name and their category name");
+
+            IQueryable<Product> products = db.Products?.Include(p => p.Category);
+
+            if ((products is null) || (!products.Any()))
+            {
+                Fail("no products found!");
+                return;
+            }
+
+            foreach (Product p in products)
+            {
+                Console.WriteLine($"{p.ProductName} is in {p.Category.CategoryName} category");
+            }
+        }
+
+    }
+}
---
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Packt.Shared;
+using System.Threading.Tasks.Dataflow;
+
+Northwind db = new Northwind();
+WriteLine($"provider:{db.Database.ProviderName}");
+
+SectionTitle("test");
+
+//QueryingCategories();
+//QueryingProducts();
+
+//var addResult = AddProduct(categoryId: 6, "Bob's Burgers", 500M);
+//var addResult = IncreaseProductPrice("Bob", 20M);
+
+var addResult = DeleteProduct("Bob");
+
+if (addResult == 1)
+{
+    Console.WriteLine($"deleted product succesfuly that starts with Bob");
+}
+
+//ListProducts(new int[] { addResult.productId });
+
+ListProducts();
